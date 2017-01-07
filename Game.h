6d9fd0bc09363c324f6c8203389833c7daf092cb
Dimitri Tolios <dtolios09@gmail.com>
2017-01-06 17:04:40 -0800

    End of Chapter 1

---
+++ Game.h
@@ -0,0 +1,35 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <SFML/Graphics.hpp>
+
+class Game : private sf::NonCopyable
+{
+public:
+	Game();
+	void					run();
+
+
+private:
+	void					processEvents();
+	void					update(sf::Time elapsedTime);
+	void					render();
+
+	void					handlePlayerInput(sf::Keyboard::Key key, bool isPressed);
+
+
+private:
+	static const float		PlayerSpeed;
+	static const sf::Time	TimePerFrame;
+
+	sf::RenderWindow		mWindow;
+	sf::Texture				mTexture;
+	sf::Sprite				mPlayer;
+
+	bool					mIsMovingUp;
+	bool					mIsMovingDown;
+	bool					mIsMovingRight;
+	bool					mIsMovingLeft;
+};
+
+#endif // GAME_H
